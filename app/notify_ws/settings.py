8ecdeb9674f14219bf8d7076b60d33aa7d17e16a
Egehan Gundogdu <egehn.gundogdu@gmail.com>
2021-05-17 10:39:21 +0300

    added channels. configured channels development server.

--- app/notify_ws/settings.py
+++ app/notify_ws/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "channels",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -69,6 +70,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'notify_ws.wsgi.application'
 
+ASGI_APPLICATION = "notify_ws.asgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
