15d84654febf651743a5c2fb814f88de120fb04b
Egehan Gundogdu <egehn.gundogdu@gmail.com>
2021-05-17 10:39:21 +0300

    added users app swapped user model for future.

--- app/notify_ws/settings.py
+++ app/notify_ws/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -130,6 +131,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "users.User"
 
 try:
     from .local_settings import *
---
+++ app/users/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+
+class User(AbstractUser):
+    pass
+
+
