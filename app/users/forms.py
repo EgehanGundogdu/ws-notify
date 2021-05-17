0753cd01bc1ddccd86d063b01d5539dc122c78a0
Egehan Gundogdu <egehn.gundogdu@gmail.com>
2021-05-17 10:39:21 +0300

    added is_staff flag and first_name last_name fields on registration form.

--- app/users/forms.py
+++ app/users/forms.py
@@ -7,3 +7,16 @@ User = get_user_model()
 class UserCreationForm(BaseUserCreationForm):
     class Meta(BaseUserCreationForm.Meta):
         model = User
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "password1",
+            "password2",
+            "is_staff",
+        ]
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.fields["first_name"].required = True
+        self.fields["last_name"].required = True
